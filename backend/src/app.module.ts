a4394547cbd31c33dd31f992cefdb3b786a50294
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-26 00:24:50 +0700

    ✨ feat: Add AppController and AppService with getHello endpoint

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
