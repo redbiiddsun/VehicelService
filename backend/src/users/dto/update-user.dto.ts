bb034ab9758a18331786b33fa64c900df8fdae92
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-26 00:48:29 +0700

    ✨ feat: Update UpdateUserDto to include validation decorators

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { User } from '@prisma/client';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+type UpdateUserType = Pick<User, 'email' | 'firstname' | 'lastname'>;
+
+export class UpdateUserDto implements UpdateUserType {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
+}
