92ffbb94b04529533913bc18cf1a440de7ea3553
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-26 00:24:08 +0700

    ✨ feat: Add Prisma module and service

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+type A<T extends string> = T extends `${infer U}ScalarFieldEnum` ? U : never;
+type Entity = A<keyof typeof Prisma>;
+type Keys<T extends Entity> = Extract<
+  keyof (typeof Prisma)[keyof Pick<typeof Prisma, `${T}ScalarFieldEnum`>],
+  string
+>;
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
+  }
+  prismaExclude<T extends Entity, K extends Keys<T>>(type: T, omit: K[]) {
+    type Key = Exclude<Keys<T>, K>;
+    type TMap = Record<Key, true>;
+    const result: TMap = {} as TMap;
+    for (const key in Prisma[`${type}ScalarFieldEnum`]) {
+      if (!omit.includes(key as K)) {
+        result[key as Key] = true;
+      }
+    }
+    return result;
+  }
+}
