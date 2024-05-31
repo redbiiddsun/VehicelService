5e7c27ba0245ed3f0c60b31e5acf20b06255b0c2
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-01 02:42:17 +0700

    feat: Add AuthModule to app imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      load: [configuration],
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
