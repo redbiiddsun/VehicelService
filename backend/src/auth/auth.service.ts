c8c1187492889dd73820524646473ad2aede38db
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-01 02:43:12 +0700

    feat: Implement login functionality with JWT authentication

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginAuthDto: LoginAuthDto) {
+    const user = await this.userService.findWithEmail(loginAuthDto.email);
+    if (!user) throw new UnauthorizedException('Invalid email or password');
+
+    const isPasswordMatch = await bcrypt.compare(loginAuthDto.password, user.password)
+    if (!isPasswordMatch) throw new UnauthorizedException('Invalid email or password');
+
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
