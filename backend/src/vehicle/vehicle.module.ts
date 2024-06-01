51aa26ceb2d58a669b020c4aa008f979c05b2102
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-01 15:18:15 +0700

    feat: Add VehicleModule for vehicle CRUD operations

---
+++ backend/src/vehicle/vehicle.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { VehicleService } from './vehicle.service';
+import { VehicleController } from './vehicle.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [VehicleController],
+  providers: [VehicleService],
+})
+export class VehicleModule {}
