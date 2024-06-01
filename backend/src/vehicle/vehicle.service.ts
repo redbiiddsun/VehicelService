98a79a5ec38ae15366bcaf45d10e4a0a60878ee0
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-01 14:57:00 +0700

    feat: Add VehicleService for vehicle CRUD operations

---
+++ backend/src/vehicle/vehicle.service.ts
@@ -0,0 +1,45 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class VehicleService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createVehicleDto: CreateVehicleDto, userId: string) {
+
+    return this.prismaService.vehicle.create({
+      data: {
+        name: createVehicleDto.name,
+        platenumber: createVehicleDto.platenumber,
+        plateprovince: createVehicleDto.plateprovince,
+        userId: userId,
+      },
+    });
+  }
+
+  findAll(userId: string) {
+    return this.prismaService.vehicle.findMany({ where: { userId } });
+  }
+
+  findWithId(id: string, userId: string) {
+    return this.prismaService.vehicle.findUnique({ where: { id, userId } });
+  }
+
+  async update(id: string, userId: string,updateVehicleDto: UpdateVehicleDto) {
+    
+    const exitiedVehicle = await this.findWithId(id, userId);
+    if(!exitiedVehicle) throw new HttpException('Vehicle not found', HttpStatus.NOT_FOUND);
+
+    return this.prismaService.vehicle.update({where: {id, userId: id}, data: updateVehicleDto});
+  }
+
+  async remove(id: string, userId: string) {
+
+    const exitiedVehicle = await this.findWithId(id, userId);
+    if(!exitiedVehicle) throw new HttpException('Vehicle not found', HttpStatus.NOT_FOUND);
+
+    return this.prismaService.vehicle.delete({where: {id, userId}});
+  }
+}
