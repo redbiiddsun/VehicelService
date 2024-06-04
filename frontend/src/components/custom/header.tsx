e69fde41ba745b7a9aa624bccb4ff55d873e5449
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-04 21:52:43 +0700

    feat: Add MountainIcon component and update header component - Add MountainIcon component to render a mountain SVG icon - Update header component to use MountainIcon component for icon rendering

---
+++ frontend/src/components/custom/header.tsx
@@ -0,0 +1,17 @@
+import { ReactElement, SVGProps } from "react";
+import { MountainIcon } from "../assets";
+
+interface IHeaderProps {
+    icon?: ReactElement<JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>>;
+    title: string;
+    description: string;
+}
+export default function HeaderComponent({icon, title, description}: IHeaderProps) {
+    return (<>
+        <div className="flex flex-col items-center space-y-2">
+            <MountainIcon className="h-8 w-8" />
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <p className="text-gray-500 dark:text-gray-400">{description}</p>
+        </div>
+    </>)
+}
