b5ff0605c27d2ef2a547bec33d240f4a1b7f3110
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-06 22:12:41 +0700

    refactor: Remove unnecessary code and update RegisterPage component

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -3,68 +3,16 @@
  * @see https://v0.dev/t/3oRqJ06mgZb
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import { Card, CardContent } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { JSX, SVGProps } from "react"
 import HeaderComponent from "@/components/custom/header"
+import RegisterForm from "@/components/custom/form/register-form"
 
 export default function RegisterPage() {
   return (
     <div className="flex min-h-[100dvh] items-center justify-center bg-gray-100 px-4 dark:bg-gray-950">
       <div className="w-full max-w-md space-y-6">
         <HeaderComponent title="Register" description="Create a new account to get started."/>
-        <Card>
-          <CardContent className="space-y-9 mt-6">
-          <div className="grid grid-cols-2 gap-4">
-          <div className="space-y-2">
-            <Label htmlFor="first-name">First name</Label>
-            <Input id="first-name" placeholder="John" required />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="last-name">Last name</Label>
-            <Input id="last-name" placeholder="Doe" required />
-          </div>
-        </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Repeat Password</Label>
-              <Input id="password" type="password" />
-            </div>
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
-          </CardContent>
-        </Card>
+        <RegisterForm />
       </div>
     </div>
   )
 }
-
-function MountainIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
-}
-
