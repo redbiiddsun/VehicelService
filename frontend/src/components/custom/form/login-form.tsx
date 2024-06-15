41a5233343dc8afeb761d97d1a8e688c80a1d42c
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-15 23:37:03 +0700

    refactor: Update login form component and add form validation using zod schema

--- frontend/src/components/custom/form/login-form.tsx
+++ frontend/src/components/custom/form/login-form.tsx
@@ -1,81 +1,86 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { axiosInstance } from "@/lib/axios";
 
+import { LoginFormValues, loginSchema } from "@/types/zod/zodSchema";
+
 const LoginForm = () => {
-	const schema = z.object({
-		email: z.string().email(),
-		password: z.string().min(8),
-	});
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+  });
 
-	type FormValues = z.infer<typeof schema>;
+  const onSubmit = async (values: LoginFormValues) => {
+    try {
+      const response = await axiosInstance.post("/auth/login", values);
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormValues>({
-		resolver: zodResolver(schema),
-	});
+      if (response.data.statusCode == 200) {
+        router.push("/dashboard");
+      } else {
+        alert("Invalid credentials");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const onSubmit = async (values: FormValues) => {
-		console.log(values);
-
-		// TODO - make a request to the backend
-		// const res = axiosInstance("", {});
-	};
-
-	return (
-		<Card>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<CardContent className="space-y-4 mt-6">
-					<div className="grid gap-[1fr,auto,1fr]">
-						<Label htmlFor="email" className="mb-2">
-							Email
-						</Label>
-						<Input
-							id="email"
-							type="email"
-							placeholder="m@example.com"
-							{...register("email")}
-						/>
-						<p className="text-destructive text-sm h-4">
-							{errors.email?.message}
-						</p>
-					</div>
-					<div className="grid gap-[1fr,auto,1fr]">
-						<Label htmlFor="password" className="mb-2">
-							Password
-						</Label>
-						<Input id="password" type="password" {...register("password")} />
-						<p className="text-destructive text-sm h-4">
-							{errors.password?.message}
-						</p>
-					</div>
-					<Button type="submit" className="w-full">
-						Sign in
-					</Button>
-				</CardContent>
-				<CardFooter className="text-center text-sm flex gap-2">
-					<p>Don't have an account?</p>
-					{/* TODO - set proper link to sign up page */}
-					<Link
-						href="#"
-						className="font-medium underline underline-offset-4"
-						prefetch={false}
-					>
-						Sign up
-					</Link>
-				</CardFooter>
-			</form>
-		</Card>
-	);
+  return (
+    <Card>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CardContent className="space-y-4 mt-6">
+          <div className="grid gap-[1fr,auto,1fr]">
+            <Label htmlFor="email" className="mb-2">
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              {...register("email")}
+            />
+            <p className="text-destructive text-sm h-4">
+              {errors.email?.message}
+            </p>
+          </div>
+          <div className="grid gap-[1fr,auto,1fr]">
+            <Label htmlFor="password" className="mb-2">
+              Password
+            </Label>
+            <Input id="password" type="password" {...register("password")} />
+            <p className="text-destructive text-sm h-4">
+              {errors.password?.message}
+            </p>
+          </div>
+          <Button type="submit" className="w-full">
+            Sign in
+          </Button>
+        </CardContent>
+        <CardFooter className="text-center text-sm flex gap-2">
+          <p>Dont have an account?</p>
+          {/* TODO - set proper link to sign up page */}
+          <Link
+            href="/signup"
+            className="font-medium underline underline-offset-4"
+            prefetch={false}
+          >
+            Sign up
+          </Link>
+        </CardFooter>
+      </form>
+    </Card>
+  );
 };
 
 export default LoginForm;
