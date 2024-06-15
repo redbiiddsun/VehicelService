3c77d2ba353458915457435d85ef74e87671a69d
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-15 23:37:39 +0700

    fix: remove extra cor to fix a cors bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
