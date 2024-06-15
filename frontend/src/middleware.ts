d1c6fa0c065b897a7ef225f6faa75c8304d4bf29
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 02:31:55 +0700

    feat: add middleware for session management and redirection

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const sessionCookie = request.cookies.get("access_token")?.name;
+  const urlPath = request.nextUrl.pathname;
+
+  // Redirect to the homepage if the user is already signed in
+  if (sessionCookie && (urlPath === "/signin" || urlPath === "/signup")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // Check if the cookie exists
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - Signin and Signup pages
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
