8b0ea4d1cc72df1e390796a1d523532a3d4399ad
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-15 23:36:54 +0700

    chore: Create SignupForm component for user registration

---
+++ frontend/src/components/custom/form/signup-form.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
+export default function SignupForm() {
+  return (
+    <>
+      <Card>
+        <CardContent className="space-y-9 mt-6">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <Label htmlFor="first-name">First name</Label>
+              <Input id="first-name" placeholder="John" required />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Doe" required />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Repeat Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Sign in
+          </Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
