f2fe59b3f1b5f0fd0d266dd3a7eacdb37d9a4e90
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 04:22:17 +0700

    feat: add signup form component and form validation using zod schema

--- frontend/src/types/zod/zodSchema.ts
+++ frontend/src/types/zod/zodSchema.ts
@@ -1,8 +1,21 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-  });
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
+
+export const signupSchema = z.object({
+  firstname: z.string().min(2),
+  lastname: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
+  repeatpassword: z.string().min(8, 'Repeat Password must be at least 8 characters long'),
+}).refine((data) => data.password === data.repeatpassword, {
+  message: "Passwords don't match",
+  path: ["repeatpassword"],
+});
+
+export type SignupFormValues = z.infer<typeof signupSchema>;
