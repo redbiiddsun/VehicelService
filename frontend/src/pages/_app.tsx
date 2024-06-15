eae54a84478c31c42206acb4663c9e55f53ee5fa
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 02:33:45 +0700

    chore: Add Toaster component for displaying toast notifications

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
       </Head>
+      
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
