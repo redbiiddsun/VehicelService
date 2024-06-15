d065c2ac4d73d334b73edbe866bd7be3efdea960
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 02:32:30 +0700

    feat: Update login form component and add form validation using zod schema

--- frontend/src/components/custom/form/login-form.tsx
+++ frontend/src/components/custom/form/login-form.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,11 +13,13 @@ import { axiosInstance } from "@/lib/axios";
 
 import { LoginFormValues, loginSchema } from "@/types/zod/zodSchema";
 
-const LoginForm = () => {
+export default function LoginForm() {
   const router = useRouter();
+  const { toast } = useToast();
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
@@ -26,13 +29,14 @@ const LoginForm = () => {
     try {
       const response = await axiosInstance.post("/auth/login", values);
 
-      if (response.data.statusCode == 200) {
-        router.push("/dashboard");
-      } else {
-        alert("Invalid credentials");
-      }
+      if (response.data.statusCode == 200) return router.push("/dashboard");
     } catch (error) {
-      console.error(error);
+      reset();
+      toast({
+        variant: "destructive",
+        title: "Invalid Email or Password",
+        description: "Please check your email and password and try again.",
+      });
     }
   };
 
@@ -69,9 +73,8 @@ const LoginForm = () => {
         </CardContent>
         <CardFooter className="text-center text-sm flex gap-2">
           <p>Dont have an account?</p>
-          {/* TODO - set proper link to sign up page */}
           <Link
-            href="/signup"
+            href="/"
             className="font-medium underline underline-offset-4"
             prefetch={false}
           >
@@ -81,6 +84,4 @@ const LoginForm = () => {
       </form>
     </Card>
   );
-};
-
-export default LoginForm;
+}
