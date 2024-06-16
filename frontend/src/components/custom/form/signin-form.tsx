0096df12b5f27fb2b9a2df9aa1dcc36bef9b4f10
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 14:57:23 +0700

    refactor: Rename LoginForm component to SigninForm

--- frontend/src/components/custom/form/signin-form.tsx
+++ frontend/src/components/custom/form/signin-form.tsx
@@ -2,19 +2,17 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import WarningBanner from "./element/warning-banner";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { axiosInstance } from "@/lib/axios";
 
-import { LoginFormValues, loginSchema } from "@/types/zod/zodSchema";
+import FormInputField from "./element/form-input";
+import { SigninFormValues, SigninSchema } from "@/types/zod/zodSchema";
 
-export default function LoginForm() {
+export default function SigninForm() {
   const router = useRouter();
   const { toast } = useToast();
   const {
@@ -22,11 +20,11 @@ export default function LoginForm() {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<LoginFormValues>({
-    resolver: zodResolver(loginSchema),
+  } = useForm<SigninFormValues>({
+    resolver: zodResolver(SigninSchema),
   });
 
-  const onSubmit = async (values: LoginFormValues) => {
+  const onSubmit = async (values: SigninFormValues) => {
     try {
       const response = await axiosInstance.post("/auth/login", values);
 
@@ -46,23 +44,24 @@ export default function LoginForm() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <CardContent className="space-y-4 mt-6">
           <div className="grid gap-[1fr,auto,1fr]">
-            <Label htmlFor="email" className="mb-2">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              {...register("email")}
+            <FormInputField<SigninFormValues>
+              id={"email"}
+              label={"email"}
+              labelstyle={"mb-2"}
+              placeholder={"m@example.com"}
+              register={register}
+              error={errors.email}
             />
-            <WarningBanner>{errors.email?.message}</WarningBanner>
           </div>
           <div className="grid gap-[1fr,auto,1fr]">
-            <Label htmlFor="password" className="mb-2">
-              Password
-            </Label>
-            <Input id="password" type="password" {...register("password")} />
-            <WarningBanner>{errors.password?.message}</WarningBanner>
+            <FormInputField<SigninFormValues>
+              id={"password"}
+              label={"password"}
+              labelstyle={"mb-2"}
+              register={register}
+              type="password"
+              error={errors.password}
+            />
           </div>
           <Button type="submit" className="w-full">
             Sign in
