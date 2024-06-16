29ad9988c081d2338a5078375bfe09ce2d733a7d
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-06-16 21:39:52 +0700

    refactor: Add @Public decorator to create method in UsersController

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Res, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UseGuards } from '@nestjs/common';
-// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Request } from 'express';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { JwtAuthGuard, Public } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
